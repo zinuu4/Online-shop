fbc9fb0dd95e33dc4451d5e85886bfdea044206a
zinuu4 <unvm7721@gmail.com>
2023-11-12 21:45:58 +0200

    feat: storing and fetching data from a file instead of variable

--- controllers/products.js
+++ controllers/products.js
@@ -17,6 +17,7 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render('shop', { products, documentTitle: 'Shop', path: '/' });
+  Product.fetchAll((products) => {
+    res.render('shop', { products, documentTitle: 'Shop', path: '/' });
+  });
 };
--- models/product.js
+++ models/product.js
@@ -1,4 +1,19 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../utils/path');
+
+const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      callback([]);
+    } else {
+      callback(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor({ title }) {
@@ -6,10 +21,15 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(callback) {
+    getProductsFromFile(callback);
   }
 };
