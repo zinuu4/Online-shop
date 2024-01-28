6f58a8b7ee9467cdb81decc2c85aff4c09f0ec39
zinuu4 <unvm7721@gmail.com>
2024-01-28 18:49:07 +0200

    feat: login page layout

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
 const { get404 } = require('./controllers/errors');
 const User = require('./models/user');
 
@@ -32,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(get404);
 
